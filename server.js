529402b86a430ee830cfa342685ee4354332ea58
ivanlalic <ivanlalic@gmail.com>
2023-12-28 00:17:16 -0300

    New Structure url+file

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const axios = require('axios');
 const cors = require('cors');
 const FormData = require('form-data');
 const fs = require('fs');
-const WebSocket = require('ws'); // Add this line
+const WebSocket = require('ws');
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 
 app.use(express.json());
 app.use(cors());
@@ -16,35 +15,32 @@ const server = app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-// Create a WebSocket server
 const wss = new WebSocket.Server({ noServer: true });
 
-// Upgrade HTTP server to support WebSocket
 server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (ws) => {
     wss.emit('connection', ws, request);
   });
 });
 
-// Handle incoming WebSocket connections
 wss.on('connection', (ws) => {
   console.log('WebSocket connection opened');
-
-  // Send a welcome message to the client
   ws.send('WebSocket connection opened');
 });
 
-// Endpoint to handle POST requests for subtitles
 app.post('/get-subtitles', async (req, res) => {
   const { audio_url, videoFile } = req.body;
   const gladiaKey = "b2640069-4ce6-41bf-a551-53b4d4d9da14";
-  const callbackUrl = "https://getsubtitlesserverv2.onrender.com/webhook"; // Update with your Render URL
+  const callbackUrl = "https://getsubtitlesserverv2.onrender.com/webhook";
 
   try {
-    let audioUrl = audio_url;
+    let audioUrl;
 
-    // If a video file is provided, upload it to Gladia and get the audio URL
-    if (videoFile) {
+    if (audio_url) {
+      // If the user shared an URL
+      audioUrl = audio_url;
+    } else if (videoFile) {
+      // If the user uploaded a video file
       const form = new FormData();
       form.append('audio', fs.createReadStream(videoFile.path));
 
@@ -58,7 +54,6 @@ app.post('/get-subtitles', async (req, res) => {
 
       audioUrl = uploadResponse.data.audio_url;
 
-      // Cleanup the temporary video file
       fs.unlink(videoFile.path, (err) => {
         if (err) {
           console.error('Error deleting temporary video file:', err);
@@ -66,9 +61,10 @@ app.post('/get-subtitles', async (req, res) => {
           console.log('Temporary video file deleted successfully');
         }
       });
+    } else {
+      throw new Error('Either audio_url or videoFile must be provided.');
     }
 
-    // Make a POST request to the Gladia API for transcription
     const transcriptionResponse = await axios.post(
       'https://api.gladia.io/v2/transcription/',
       {
@@ -88,14 +84,12 @@ app.post('/get-subtitles', async (req, res) => {
       }
     );
 
-    // Send the transcription response to the client via WebSocket
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(transcriptionResponse.data));
       }
     });
 
-    // Send a response to the original HTTP request
     res.json(transcriptionResponse.data);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
@@ -103,12 +97,9 @@ app.post('/get-subtitles', async (req, res) => {
   }
 });
 
-// Endpoint to handle incoming webhook notifications
 app.post('/webhook', (req, res) => {
-  // Handle the webhook response here
   console.log('Webhook Notification Received:', req.body);
 
-  // Send the webhook response to all connected clients via WebSocket
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       client.send(JSON.stringify(req.body));
